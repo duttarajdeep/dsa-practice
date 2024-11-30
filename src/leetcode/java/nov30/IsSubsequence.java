660b727726b7f5e3ec63b9df6fe6c0a96af968bf
Rajdeep Dutta <rajdeep.dutta.5007@gmail.com>
2024-11-30 11:56:28 +0530

    DSA

---
+++ src/leetcode/java/nov30/IsSubsequence.java
@@ -0,0 +1,16 @@
+package leetcode.java.nov30;
+
+public class IsSubsequence {
+    public static void main(String[] args) {
+        System.out.println(isSubsequence("ab", "baab"));
+    }
+
+    public static boolean isSubsequence(String s, String t) {
+        int i = 0, j = 0;
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) i++;
+            j++;
+        }
+        return i == s.length();
+    }
+}
