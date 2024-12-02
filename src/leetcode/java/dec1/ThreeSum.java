a5628610e9b5c2706ca9231cf9cb0ff60a711dcf
Rajdeep Dutta <rajdeep.dutta.5007@gmail.com>
2024-12-02 09:34:53 +0530

    DSA

---
+++ src/leetcode/java/dec1/ThreeSum.java
@@ -0,0 +1,43 @@
+package leetcode.java.dec1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeSum {
+
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        threeSum(nums);
+    }
+
+    static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
+                int low = i + 1;
+                int high = nums.length - 1;
+                int sum = -nums[i];
+                while (low < high) {
+                    if (nums[low] + nums[high] == sum) {
+                        result.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                        while (low < high && nums[low] == nums[low + 1])
+                            low++;
+                        while (low < high && nums[high] == nums[high - 1])
+                            high--;
+                        low++;
+                        high--;
+                    } else if (nums[low] + nums[high] > sum) {
+                        high--;
+                    } else {
+                        low++;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+}
